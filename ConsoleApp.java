aaa48e45f9dd4a0645cf04bc6da7c0d966e19d64
Kutay Görür <gorurkutay@gmail.com>
2026-01-28 21:10:01 +0300

    Edited main and added methods to display the menu and ensure valid input
    Methods Added: inputValidifier(), displayMenu()

--- ConsoleApp.java
+++ ConsoleApp.java
@@ -1,22 +1,78 @@
 import java.util.Random;
+import java.util.Scanner;
 
-public class ConsoleApp{
+public class ConsoleApp {
+    public static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         int size = 10;
         int[] arr = randomArrayGenerator(size);
         // all the work will be shown in here
+
+        boolean canContinue = true;
+        while (canContinue) {
+            displayMenu();
+            int decision = inputValidifier("Please select an operation (1-4): ");
+
+            switch (decision) {
+                case 1:
+                    // TODO: call the minimum and maximum method and properly display
+                    break;
+                case 2:
+                    differenceFromAverage(arr);
+                    break;
+                case 3:
+                    System.out.printf(
+                            "Sum of elements with odd numbered indexes: %d\nSum of elements with even numbered indexes: %d\n",
+                            sumOfOddIndexElements(arr), sumOfEvenIndexElements(arr));
+                    break;
+                case 4:
+                    System.out.println("Program ended.");
+                    canContinue = false;
+                    break;
+                default:
+                    System.out.println("Invalid operation!");
+                    break;
+            }
+        }
     }
 
-    public static int[] randomArrayGenerator(int size)
-    {
+    public static void displayMenu() {
+        System.out.println("""
+                \n=====[!] MENU [!]=====
+                1) Find and display the minimum and maximum values in the array
+                2) Find the average and display how each element of the array differs from the average
+                3) Find and display the sum of elements with odd- and even-numbered indexes
+                4) EXIT
+                ======================""");
+    }
+
+    public static int inputValidifier(String prompt) {
+        boolean isValid = false;
+        int result = -1; // to get things started
+
+        while (!isValid) {
+            System.out.print(prompt);
+            if (sc.hasNextInt()) {
+                int input = sc.nextInt();
+                if (input >= 1 && input <= 4) {
+                    result = input;
+                    isValid = true;
+                }
+            } else {
+                System.out.println("Invalid input, try again!");
+            }
+            sc.nextLine();
+        }
+        return result;
+    }
+
+    public static int[] randomArrayGenerator(int size) {
         Random random = new Random();
         // array is being created in the given size
         int[] randomArray = new int[size];
 
-        for(int i = 0; i< size;i++)
-        {
+        for (int i = 0; i < size; i++) {
             // array is being filled with random numbers from 0 to 100
             randomArray[i] = random.nextInt(101);
         }
@@ -25,8 +81,7 @@ public class ConsoleApp{
 
     public static double averageArray(int[] arr) {
         double sum = 0;
-        for (int x : arr) 
-        {
+        for (int x : arr) {
             sum += x;
         }
         return sum / arr.length;
@@ -35,22 +90,23 @@ public class ConsoleApp{
     public static void differenceFromAverage(int[] arr) {
         double average = averageArray(arr);
         System.out.print("Differences from average: ");
-        for (int i = 0; i < arr.length; i++) 
-        {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print((arr[i] - average) + " ");
         }
         System.out.println();
     }
 
     /**
-     * This function takes an integer array and loops over it by increasing the index 2 at a time to get elements with even indexes starting with index 0.
+     * This function takes an integer array and loops over it by increasing the
+     * index 2 at a time to get elements with even indexes starting with index 0.
+     * 
      * @param arr int[]
-     * @return Returns the sum of elemets in an int array with even index.
+     * @return Returns the sum of elements in an int array with even index.
      */
-    public static int sumOfEvenIndexElements(int [] arr) {
+    public static int sumOfEvenIndexElements(int[] arr) {
         int sum = 0;
 
-        for (int i = 0; i < arr.length; i = i + 2){
+        for (int i = 0; i < arr.length; i = i + 2) {
             sum += arr[i];
         }
 
@@ -58,14 +114,16 @@ public class ConsoleApp{
     }
 
     /**
-     * This function takes an integer array and loops over it by increasing the index 2 at a time to get elements with odd indexes starting with index 1.
+     * This function takes an integer array and loops over it by increasing the
+     * index 2 at a time to get elements with odd indexes starting with index 1.
+     * 
      * @param arr int[]
-     * @return Returns the sum of elemets in an int array with even index.
+     * @return Returns the sum of elements in an int array with odd index.
      */
-    public static int sumOfOddIndexElements(int [] arr) {
+    public static int sumOfOddIndexElements(int[] arr) {
         int sum = 0;
 
-        for (int i = 1; i < arr.length; i = i + 2){
+        for (int i = 1; i < arr.length; i = i + 2) {
             sum += arr[i];
         }
 
